bb4d514c29b1a8077a2b77dc9f9ac6e38d438287
ben <ben@e959a6d6-1924-0410-92b3-a6fa492f4c66>
2007-09-12 11:34:03 +0000

    moved controller filters to sandstone_system, where they belong
    git-svn-id: http://svn.extendviget.com/lab/trunk/plugins/sandstone@316 e959a6d6-1924-0410-92b3-a6fa492f4c66

--- generators/sandstone/templates/helpers/sandstone_helper.rb
+++ generators/sandstone/templates/helpers/sandstone_helper.rb
@@ -2,31 +2,4 @@ module SandstoneHelper
   def editor
     logged_in? && current_user ? current_user.editor : nil
   end
-
-  private
-  def require_sandstone_editor
-    if !logged_in? || !editor
-      flash[:notice] = 'You must be logged in as an editor for that'
-      access_denied
-      false
-    end
-  end
-
-  def require_sandstone_manager
-    if !logged_in? || !editor || !editor.manager?
-      flash[:notice] = 'You must be logged in as a manager for that'
-      access_denied
-      false
-    end
-  end
-
-  def authorize_sandstone_editor
-    @page = Page.find(params[:id])
-    if editor.can_edit? @page
-      yield
-    else
-      flash[:notice] = 'You are not authorized to edit that page'
-      redirect_to pages_path
-    end
-  end
 end
